ea1a68be13fe862379f0db7e0e26b60a9218bafa
Matt Ruffner <mattruffner7@gmail.com>
2016-02-10 10:09:54 -0500

    now performs texture mapping from specified 24bit bmp file

--- img2globe.py
+++ img2globe.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+import sys
 import numpy as np
 import Image
 
@@ -8,11 +9,22 @@ ny = 20
 
 rmat = [[1.,0,0],[0,0,-1.],[0,1.,0]]
 
+tmap = 0
+
+if len(sys.argv) > 1:
+    print "  using",sys.argv[1]," as tex map"
+    
+    tmap = Image.open(sys.argv[1])
+    print "    width: ",tmap.size[0]
+    print "    height:",tmap.size[1]
+
 for x in range(nx):
-    lat = 2 * np.pi * ((x+(1./nx)) / nx) - np.pi
+    percx = ((x+(1./nx)) / nx)
+    lat = 2 * np.pi * percx - np.pi
 
     for y in range(ny):
-        lon = np.pi * ((y+(1./ny)) / ny) 
+        percy = ((y+(1./ny)) / ny)
+        lon = np.pi * percy 
 
         p = [np.cos(lat)*np.sin(lon), 
              -1*np.cos(lon),
@@ -22,6 +34,6 @@ for x in range(nx):
         for row in rmat:
             new_p.append(np.dot(row, p))
 
-        print new_p[0]," ",new_p[1]," ",new_p[2]
+        print new_p[0], new_p[1], new_p[2], tmap.getpixel((percx*tmap.size[0],percy*tmap.size[1]))
 
         #print p[0],"\t",p[1],"\t",p[2]
