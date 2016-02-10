702ac1eab06a04ba7975b7a6149f7ce0217e6e56
Matt Ruffner <mattruffner7@gmail.com>
2016-02-10 11:10:08 -0500

    added matplotlib visualization of texture map

--- img2globe.py
+++ img2globe.py
@@ -3,13 +3,33 @@
 import sys
 import numpy as np
 import Image
+import matplotlib.pyplot as plt
 
-nx = 20
-ny = 20
 
 rmat = [[1.,0,0],[0,0,-1.],[0,1.,0]]
 
-tmap = 0
+def gen_points(w,h,tex_map):
+    points = []
+    for x in range(w):
+        percx = ((x+(1./w)) / w)
+        lat = 2 * np.pi * percx - np.pi
+
+        for y in range(h):
+            percy = ((y+(1./h)) / h)
+            lon = np.pi * percy 
+
+            p = [np.cos(lat)*np.sin(lon), 
+                 -1*np.cos(lon),
+                 -1* np.sin(lat)*np.sin(lon)]
+
+            new_p = []
+            for row in rmat:
+                new_p.append(np.dot(row, p))
+
+            points.append([new_p[0], new_p[1], new_p[2], tex_map.getpixel((percx*tex_map.size[0],percy*tex_map.size[1]))])
+    return points
+
+
 
 if len(sys.argv) > 1:
     print "  using",sys.argv[1]," as tex map"
@@ -17,23 +37,11 @@ if len(sys.argv) > 1:
     tmap = Image.open(sys.argv[1])
     print "    width: ",tmap.size[0]
     print "    height:",tmap.size[1]
+    
+    pts = gen_points(144,72,tmap)
 
-for x in range(nx):
-    percx = ((x+(1./nx)) / nx)
-    lat = 2 * np.pi * percx - np.pi
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
 
-    for y in range(ny):
-        percy = ((y+(1./ny)) / ny)
-        lon = np.pi * percy 
-
-        p = [np.cos(lat)*np.sin(lon), 
-             -1*np.cos(lon),
-             -1* np.sin(lat)*np.sin(lon)]
-
-        new_p = []
-        for row in rmat:
-            new_p.append(np.dot(row, p))
-
-        print new_p[0], new_p[1], new_p[2], tmap.getpixel((percx*tmap.size[0],percy*tmap.size[1]))
-
-        #print p[0],"\t",p[1],"\t",p[2]
+    for x,y,z,color in pts:
+        ax.scatter3D(x, y, z, c=color)
