bc9544397ea74841323be651ed5fa18edc3bc0e4
Matt Ruffner <mattruffner7@gmail.com>
2016-02-17 16:33:58 -0500

    added prelim base64 decoding for image upload

--- orbit-portal/src/igen.php
+++ orbit-portal/src/igen.php
@@ -1,11 +1,20 @@
 <?php
 
-if( isset($_POST['newText']) ) {
+if( isset($_POST['newText']) ){
    
    shell_exec("sh -e /home/matt/Documents/globe-clock/text2image.sh \"" . $_POST['newText'] . "\" /tmp/out.bmp");
    shell_exec("python /home/matt/Documents/globe-clock/img2globe-nox.py /tmp/out.bmp /tmp/out.asc");
    shell_exec("cp /tmp/out.asc /var/www/html/orbit-portal/clouds/out.asc");
 
    header("Location: ../");
+}
+
+if( isset($_POST['fromImage']) ){
+    $bmpData = base64_decode($_POST['fromImage']);
+
+
+    file_put_contents("/tmp/in.bmp", $bmpData);
+    shell_exec("python /home/matt/Documents/globe-clock/img2globe-nox.py /tmp/in.bmp /tmp/out.asc");
+    shell_exec("cp /tmp/out.asc /var/www/html/orbit-portal/clouds/out.asc");
 }
 ?>
